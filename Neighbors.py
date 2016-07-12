5854c1442e8596efa7118380541b3964565aba22
necropaz <jan-huber@hispeed.ch>
2016-07-12 19:23:50 +0200

    V0_1

---
+++ Neighbors.py
@@ -0,0 +1,48 @@
+import urllib2
+import json
+
+command = {'command': 'getNeighborsActivity'}
+
+stringified = json.dumps(command)
+
+headers = {'content-type': 'application/json'}
+
+request = urllib2.Request(url="http://localhost:14265", data=stringified, headers=headers)
+returnData = urllib2.urlopen(request).read()
+
+neighbors = json.loads(returnData)
+length=len(neighbors['neighbors'])
+for i in range (0,length):
+	print("IP:"+str(neighbors['neighbors'][i]['node'])+"LatestPacketReceived:"+str(neighbors['neighbors'][i]['latestPacketReceived']))
+
+respond=raw_input("Should I delete not responding Neighbors? [y/n]")
+while True:
+	if respond=="y":
+		#filepath=raw_input("Where is the file locatet? ")
+		filepath='IRI.cfg'
+		f = open(filepath, 'r+')
+		#print(f.read())
+		lines=f.readlines()
+		#print(lines)
+		for i in range (0,length):
+			if str(neighbors['neighbors'][i]['latestPacketReceived'])=='None':
+				ip=str(neighbors['neighbors'][i]['node'])
+				ip.replace(ip[:4], '+udp://')
+				print(ip)
+				for x in range(0,len(lines)-1):
+					if ip in lines[x-1]:
+						del lines[x-1]
+						print("deletet line:"+str(x))
+
+		f.close()
+		f=open(filepath, 'w')
+		f.write("".join(lines))
+		f.close()
+		print("Succesfully delet bad Neighbors!")
+		break
+	elif respond=="n":
+		exit()
+		break
+	else:
+		print("No correct Input.")
+		respond=raw_input("Should I delete not responding Neighbors? [y/n]")
